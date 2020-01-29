b10ab8ca0a297e4b17e701ef67e5ea77ebe5d47d
Fernando Romero <A01335834@itesm.mx>
2020-01-29 08:51:05 -0600

    ultima

--- Parte-1/js/index.js
+++ Parte-1/js/index.js
@@ -20,24 +20,34 @@ function watchForm(){
 function displayResults(responseJSON){
 
     let submit = $("#submit");
+    let buscar  = $("#query").val();
+    let resultado = $("#resultado");
 
     submit.on("click", (event) =>{
         event.preventDefault();
         
         
+        console.log(buscar);
         console.log(responseJSON);
 
-        let resultado = $("#resultado");
-     
-        for(let i = 0; i<responseJSON.length; i++ ){
-            resultado.append(`<div><h3>Nombre: ${responseJSON[i].name}</h3>
-                                    <h3>Capital: ${responseJSON[i].capital}</h3>
-                                    <h3>Poblacion: ${responseJSON[i].population}</h3>
-                                    <h3>Region: ${responseJSON[i].region}</h3>
-                                    <h3>Zonas horarias: ${responseJSON[i].timezones}</h3>
-                                    <h3>Paises colindadas: ${responseJSON[i].borders[i]}</h3></div>`);
+        
+        if (buscar != null){
+            for(let i = 0; i<responseJSON.length; i++ ){
+                resultado.append(`<div><h3>Nombre: ${responseJSON[i].name}</h3>
+                                        <h3>Capital: ${responseJSON[i].capital}</h3>
+                                        <h3>Poblacion: ${responseJSON[i].population}</h3>
+                                        <a><img src="${responseJSON[i].translations[4]}" /></a>
+                                        <h3>Region: ${responseJSON[i].region}</h3>
+                                        <h3>Zonas horarias: ${responseJSON[i].timezones}</h3>
+                                        <h3>Paises colindadas: ${responseJSON[i].borders[i]}</h3></div>`);
+    
+            }
 
+        }else{
+            console.log("No hay ciudad");
         }
+     
+
         
     
     });
@@ -49,33 +59,10 @@ function displayResults(responseJSON){
 
 };
 
-/*
-
-function button (){
-
-    let submit = $("#submit");
-
-    submit.on("click", (event) =>{
-        event.preventDefault();
-        
-        console.log(responseJSON);
-        console.log("entro");
-    
-    });
-    
-
-
-
-}
-*/
-
 
 function init () {
 
-
-// button();
  watchForm();
-
 
 }
 
--- Parte-2/model.js
+++ Parte-2/model.js
@@ -44,5 +44,5 @@ let BookmarkList = {
 
 
 module.exports = {
-    
+    BookmarkList
 };
